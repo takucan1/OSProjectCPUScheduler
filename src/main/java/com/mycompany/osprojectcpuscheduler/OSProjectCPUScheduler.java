f170efe54a0bc206be4bb9847ebb4a48db3e3046
Takucan <Takucan@WIN-Q47ITF59IRK>
2025-07-12 23:55:10 +0800

    Test

--- src/main/java/com/mycompany/osprojectcpuscheduler/OSProjectCPUScheduler.java
+++ src/main/java/com/mycompany/osprojectcpuscheduler/OSProjectCPUScheduler.java
@@ -11,6 +11,6 @@ package com.mycompany.osprojectcpuscheduler;
 public class OSProjectCPUScheduler {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("TEST!");
     }
 }
